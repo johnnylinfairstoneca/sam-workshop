360534446c7bd05bd5132af57b42b547d5a07b72
EC2 Default User <ec2-user@ip-172-31-40-250.ec2.internal>
2023-04-05 20:34:09 +0000

    Breaking the lambda function on purpose

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -24,17 +23,17 @@ type OutputMessage struct {
 }
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	panic("This will cause a deployment rollback")
+	// output := OutputMessage{
+	// 	Message: "I'm using canary deployments",
+	// }
 
-	output := OutputMessage{
-		Message: "I'm using canary deployments",
-	}
+	// outputStr, _ := json.Marshal(output)
 
-	outputStr, _ := json.Marshal(output)
-
-	return events.APIGatewayProxyResponse{
-		Body:       string(outputStr),
-		StatusCode: 200,
-	}, nil
+	// return events.APIGatewayProxyResponse{
+	// 	Body:       string(outputStr),
+	// 	StatusCode: 200,
+	// }, nil
 }
 
 func main() {
